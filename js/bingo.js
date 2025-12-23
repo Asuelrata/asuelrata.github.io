d1574ccf97c0791811000596539a544b5c876bcc
asiers@inteman.com <asiers@inteman.com>
2025-12-23 15:28:31 +0100

    Arreglo con extensiones y poner nombre a archivo descargado

--- js/bingo.js
+++ js/bingo.js
@@ -102,6 +102,8 @@ function generarcanvas()
     canvas.style.border = "1px solid black";
     canvas.style.display = "block";
 
-    var imgdownload = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.  
-    window.location.href=imgdownload;
+    var link = document.createElement('a');
+    link.setAttribute('download', 'bingopokeñol.png');
+    link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+    link.click();
 }
